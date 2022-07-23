7d36823e75a1f6e593ca3d37a13ea9691a42746f
Bartek Z <bartekzarzyk@gmail.com>
2022-07-23 16:09:40 +0200

    add old code

---
+++ contact-form.php
@@ -0,0 +1,17 @@
+<!-- <?php
+
+
+// $name = utf8_encode(htmlentities($_POST['name'], ENT_QUOTES, "UTF-8"));
+// $email = utf8_encode(htmlentities($_POST['email'], ENT_QUOTES, "UTF-8"));
+// $message = utf8_encode(htmlentities($_POST['message'], ENT_QUOTES, "UTF-8"));
+// $formcontent="From: $name \n Message: $message";
+// $recipient = "bartek@buaa.pl";
+// $subject = "Wiadomość z formularza";
+// $mailheader = "From: $email \r\n";
+
+
+// mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+// echo "Dziękujemy za kontakt!";
+
+// ?>
+ -->
